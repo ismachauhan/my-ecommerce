3406ae8cf6f04cdc450098f499d7ecb0c61a095c
Isma Chauhan <ismachauhan8@gmail.com>
2025-06-28 18:51:47 +0530

    Finalize Django e-commerce site without payment gateway

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,4 +1,4 @@
-// Get CSRF token from cookies
+// Get CSRF token
 function getCookie(name) {
     let cookieValue = null;
     if (document.cookie && document.cookie !== '') {
@@ -14,30 +14,64 @@ function getCookie(name) {
     return cookieValue;
 }
 
-
-// Declare CSRF token globally once
 const csrftoken = getCookie('csrftoken');
-function addCookieItem(productId, action){
-	console.log('User is not authenticated')
 
-	if (action == 'add'){
-		if (cart[productId] == undefined){
-		cart[productId] = {'quantity':1}
+// Event listener for all update-cart buttons
+let updateBtns = document.getElementsByClassName('update-cart');
 
-		}else{
-			cart[productId]['quantity'] += 1
-		}
-	}
+for (let i = 0; i < updateBtns.length; i++) {
+    updateBtns[i].addEventListener('click', function () {
+        let productId = this.dataset.product;
+        let action = this.dataset.action;
+        console.log('productId:', productId, 'action:', action);
 
-	if (action == 'remove'){
-		cart[productId]['quantity'] -= 1
+        if (user === 'AnonymousUser') {
+            addCookieItem(productId, action);
+        } else {
+            updateUserOrder(productId, action);
+        }
+    });
+}
 
-		if (cart[productId]['quantity'] <= 0){
-			console.log('Item should be deleted')
-			delete cart[productId];
-		}
-	}
-	console.log('CART:', cart)
-	document.cookie ='cart=' + JSON.stringify(cart) + ";domain=;path=/"
-	location.reload()
+// For authenticated users
+function updateUserOrder(productId, action) {
+    fetch('/update_item/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken
+        },
+        body: JSON.stringify({ 'productId': productId, 'action': action })
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Data:', data);
+            location.reload();
+        });
+}
+
+// For guest users
+function addCookieItem(productId, action) {
+    console.log('User is not authenticated');
+
+    if (action === 'add') {
+        if (cart[productId] === undefined) {
+            cart[productId] = { 'quantity': 1 };
+        } else {
+            cart[productId]['quantity'] += 1;
+        }
+    }
+
+    if (action === 'remove') {
+        cart[productId]['quantity'] -= 1;
+
+        if (cart[productId]['quantity'] <= 0) {
+            console.log('Item should be deleted');
+            delete cart[productId];
+        }
+    }
+
+    document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/";
+    console.log('Cart:', cart);
+    location.reload();
 }
--- store/views.py
+++ store/views.py
@@ -2,21 +2,19 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import json
 import datetime
-from .utils import cartData, cookieCart
-
-
+from .utils import cartData
 from .models import *
-
 
 def store(request):
     if request.user.is_authenticated:
         customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        order = Order.objects.filter(customer=customer, complete=False).first()
+        if not order:
+            order = Order.objects.create(customer=customer, complete=False)
         items = order.orderitem_set.all()
-        cartItems = order.get_cart_items
+        cartItems = order.get_cart_items  # ✅ fixed
     else:
         data = cartData(request)
-
         cartItems = data['cartItems']
         order = data['order']
         items = data['items']
@@ -25,16 +23,16 @@ def store(request):
     context = {'products': products, 'cartItems': cartItems}
     return render(request, 'store/store.html', context)
 
-
 def cart(request):
     if request.user.is_authenticated:
         customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        order = Order.objects.filter(customer=customer, complete=False).first()
+        if not order:
+            order = Order.objects.create(customer=customer, complete=False)
         items = order.orderitem_set.all()
-        cartItems = order.get_cart_items
+        cartItems = order.get_cart_items  # ✅ fixed
     else:
         data = cartData(request)
-
         cartItems = data['cartItems']
         order = data['order']
         items = data['items']
@@ -42,24 +40,22 @@ def cart(request):
     context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'store/cart.html', context)
 
-
-
 def checkout(request):
     if request.user.is_authenticated:
         customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        order = Order.objects.filter(customer=customer, complete=False).first()
+        if not order:
+            order = Order.objects.create(customer=customer, complete=False)
         items = order.orderitem_set.all()
-        cartItems = order.get_cart_items
+        cartItems = order.get_cart_items  # ✅ fixed
     else:
         data = cartData(request)
-
         cartItems = data['cartItems']
         order = data['order']
         items = data['items']
 
     context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'store/checkout.html', context)
-
 
 def updateItem(request):
     data = json.loads(request.body)
@@ -70,7 +66,9 @@ def updateItem(request):
 
     customer = request.user.customer
     product = Product.objects.get(id=productId)
-    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+    order = Order.objects.filter(customer=customer, complete=False).first()
+    if not order:
+        order = Order.objects.create(customer=customer, complete=False)
 
     orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
 
@@ -87,78 +85,52 @@ def updateItem(request):
     return JsonResponse('Item was added', safe=False)
 
 def processOrder(request):
-	transaction_id = datetime.datetime.now().timestamp()
-	data = json.loads(request.body)
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
 
-	if request.user.is_authenticated:
-		customer = request.user.customer
-		order, created = Order.objects.get_or_create(customer=customer, complete=False)
-	else:
-		print('User is not logged in')
-		print('COOKIES:', request.COOKIES)
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order = Order.objects.filter(customer=customer, complete=False).first()
+        if not order:
+            order = Order.objects.create(customer=customer, complete=False)
+    else:
+        print('User is not logged in')
+        print('COOKIES:', request.COOKIES)
+        name = data['form']['name']
+        email = data['form']['email']
 
-		name = data['form']['name']
-		email = data['form']['email']
+        cookieData = cookieCart(request)
+        items = cookieData['items']
 
-		cookieData = cookieCart(request)
-		items = cookieData['items']
+        customer, created = Customer.objects.get_or_create(email=email)
+        customer.name = name
+        customer.save()
 
-		customer, created = Customer.objects.get_or_create(email=email)
-		customer.name = name
-		customer.save()
+        order = Order.objects.create(customer=customer, complete=False)
 
-		order = Order.objects.create(customer=customer, complete=False)
+        for item in items:
+            product = Product.objects.get(id=item['id'])
+            orderItem = OrderItem.objects.create(
+                product=product,
+                order=order,
+                quantity=item['quantity'],
+            )
 
-		for item in items:
-			product = Product.objects.get(id=item['id'])
-			orderItem = OrderItem.objects.create(
-				product=product,
-				order=order,
-				quantity=item['quantity'],
-			)
+    total = float(data['form']['total'])
+    order.transaction_id = transaction_id
 
-	total = float(data['form']['total'])
-	order.transaction_id = transaction_id
+    if total == order.get_cart_total:  # ✅ assuming this is also a @property
+        order.complete = True
+    order.save()
 
-	if total == order.get_cart_total:
-		order.complete = True
-	order.save()
+    if order.shipping == True:
+        ShippingAddress.objects.create(
+            customer=customer,
+            order=order,
+            address=data['shipping']['address'],
+            city=data['shipping']['city'],
+            state=data['shipping']['state'],
+            zipcode=data['shipping']['zipcode'],
+        )
 
-	if order.shipping == True:
-		ShippingAddress.objects.create(
-			customer=customer,
-			order=order,
-			address=data['shipping']['address'],
-			city=data['shipping']['city'],
-			state=data['shipping']['state'],
-			zipcode=data['shipping']['zipcode'],
-		)
-
-	return JsonResponse('Payment complete', safe=False)
-
-def guestOrder(request, data):
-	name = data['form']['name']
-	email = data['form']['email']
-
-	cookieData = cookieCart(request)
-	items = cookieData['items']
-
-	customer, created = Customer.objects.get_or_create(
-			email=email,
-			)
-	customer.name = name
-	customer.save()
-
-	order = Order.objects.create(
-		customer=customer,
-		complete=False,
-		)
-
-	for item in items:
-		product = Product.objects.get(id=item['id'])
-		orderItem = OrderItem.objects.create(
-			product=product,
-			order=order,
-			quantity=item['quantity'],
-		)
-	return customer, order
+    return JsonResponse('Payment complete', safe=False)
